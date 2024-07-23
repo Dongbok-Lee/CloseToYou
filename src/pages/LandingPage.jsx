2484841c2a175b3e8be27510137bb0e481b5e76a
백하람 <developer.venice@gmail.com>
2024-07-23 17:36:49 +0900

    init: 프론트엔드 프로젝트 세팅

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <React.Fragment>
+      <heeder></heeder>
+      <div>page</div>
+      <header></header>;
+    </React.Fragment>
+  );
+};
+
+export default LandingPage;
