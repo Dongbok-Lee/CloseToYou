ae5445cbabdc5d6cca939e8f82f229d1e1e18f50
이수민 <gogotnals@naver.com>
2024-07-31 17:33:46 +0900

    feat: Clothes Enum 작성

--- backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/domain/Pattern.java
+++ backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/domain/Pattern.java
@@ -1,4 +1,14 @@
 package ssafy.closetoyou.clothes.domain;
 
 public enum Pattern {
+    ANIMAL,
+    ARTIFACT,
+    CHECK,
+    DOT,
+    ETC,
+    ETCNATURE,
+    GEOMETRIC,
+    PLANTS,
+    STRIPE,
+    SYMBOL
 }
