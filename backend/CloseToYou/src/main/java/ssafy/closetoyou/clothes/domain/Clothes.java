11707e0cd4127d03d15614069d4f3ed777898ee8
이수민 <gogotnals@naver.com>
2024-07-31 17:11:44 +0900

    feat: Clothes 도메인 생성

---
+++ backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/domain/Clothes.java
@@ -0,0 +1,19 @@
+package ssafy.closetoyou.clothes.domain;
+
+import java.time.LocalDateTime;
+
+public class Clothes {
+    private Long clothesId;
+    private String nickname;
+    private Type type;
+    private Pattern pattern;
+    private Color color;
+    private String size;
+    private String texture;
+    private String memo;
+    private Long wearingCount;
+    private String location;
+    private Long closetId;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime updatedDateTime;
+}
