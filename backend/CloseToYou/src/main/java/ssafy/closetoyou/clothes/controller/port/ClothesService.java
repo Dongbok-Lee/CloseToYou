f5a1d4e80d9623ca493db1a3b6e0e56ea5bcab0d
이수민 <gogotnals@naver.com>
2024-08-01 14:25:27 +0900

    feat: ClothesService 기능 개발

--- backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/controller/port/ClothesService.java
+++ backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/controller/port/ClothesService.java
@@ -1,19 +1,18 @@
 package ssafy.closetoyou.clothes.controller.port;
+import ssafy.closetoyou.clothes.controller.request.ClothesUpdateRequest;
 import ssafy.closetoyou.clothes.controller.response.ClothesResponse;
-import ssafy.closetoyou.clothes.domain.ClothesRequestDto;
-import ssafy.closetoyou.clothes.domain.SearchFilter;
+import ssafy.closetoyou.clothes.controller.request.ClothesRequest;
+import ssafy.closetoyou.clothes.controller.request.ClothesCondition;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ClothesService {
-    Long addClothes(ClothesRequestDto clothesRequestDto);
-    Long updateClothes(ClothesRequestDto clothesRequestDto);
-    Long updateClothesLocation(ClothesRequestDto clothesRequestDto);
-    Long deleteClothes(Long clothesId);
+    Long addClothes(ClothesRequest clothesRequest);
+    Long updateClothes(Long clothesId, ClothesUpdateRequest clothesUpdateRequest);
+    Long removeClothes(Long clothesId);
 
-    Optional<ClothesResponse> findClothes(Long clothesId);
-    Optional<List<ClothesResponse>> findAllClothes();
-    Optional<List<ClothesResponse>> searchClothesBySearchFilter(SearchFilter searchFilter);
-    Optional<List<ClothesResponse>> searchClothesBySearchKeyword(String searchKeyword);
+    ClothesResponse findClothes(Long clothesId);
+    List<ClothesResponse> findAllClothes();
+    List<ClothesResponse> searchClothesByClothesCondition(ClothesCondition clothesCondition);
+    List<ClothesResponse> searchClothesBySearchKeyword(String searchKeyword);
 }
--- backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/domain/Clothes.java
+++ backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/domain/Clothes.java
@@ -1,7 +1,13 @@
 package ssafy.closetoyou.clothes.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import ssafy.closetoyou.clothes.controller.request.ClothesUpdateRequest;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
 public class Clothes {
     private Long clothesId;
     private String nickname;
@@ -16,4 +22,31 @@ public class Clothes {
     private Long closetId;
     private LocalDateTime createdDateTime;
     private LocalDateTime updatedDateTime;
+    private LocalDate lastWornDate;
+
+    @Builder
+    public Clothes(Long clothesId, String nickname, Type type, Pattern pattern, Color color, String size, String texture, String memo, Long wearingCount, String location, Long closetId, LocalDateTime createdDateTime, LocalDateTime updatedDateTime, LocalDate lastWornDate) {
+        this.clothesId = clothesId;
+        this.nickname = nickname;
+        this.type = type;
+        this.pattern = pattern;
+        this.color = color;
+        this.size = size;
+        this.texture = texture;
+        this.memo = memo;
+        this.wearingCount = wearingCount;
+        this.location = location;
+        this.closetId = closetId;
+        this.createdDateTime = createdDateTime;
+        this.updatedDateTime = updatedDateTime;
+        this.lastWornDate = lastWornDate;
+    }
+
+    public void changeClothesInfo(ClothesUpdateRequest clothesUpdateRequest) {
+        this.nickname = clothesUpdateRequest.getNickname();
+        this.type = Type.valueOf(clothesUpdateRequest.getType());
+        this.pattern = Pattern.valueOf(clothesUpdateRequest.getPattern());
+        this.color = Color.valueOf(clothesUpdateRequest.getColor());
+        this.memo = clothesUpdateRequest.getMemo();
+    }
 }
