5c410a65de516abe58357af8fb9fb9ec4ee2b043
이수민 <gogotnals@naver.com>
2024-08-01 14:23:52 +0900

    refactor: Clothes Request, Response model로 변환 메서드 추가

--- backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/controller/request/ClothesRequest.java
+++ backend/CloseToYou/src/main/java/ssafy/closetoyou/clothes/controller/request/ClothesRequest.java
@@ -1,11 +1,37 @@
 package ssafy.closetoyou.clothes.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ssafy.closetoyou.clothes.domain.Clothes;
+import ssafy.closetoyou.clothes.domain.Color;
+import ssafy.closetoyou.clothes.domain.Pattern;
+import ssafy.closetoyou.clothes.domain.Type;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class ClothesRequest {
-    private Long clothesId;
+    @NotBlank
     private String nickname;
+    @NotBlank
     private String type;
+    @NotBlank
     private String pattern;
+    @NotBlank
     private String color;
     private String location;
+    @NotBlank
     private Long closetId;
+
+    public Clothes toModel() {
+        return Clothes.builder()
+                .nickname(nickname)
+                .type(Type.valueOf(type))
+                .pattern(Pattern.valueOf(pattern))
+                .color(Color.valueOf(color))
+                .location(location)
+                .closetId(closetId)
+                .build();
+    }
 }
