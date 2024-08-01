db124b77afae7a4e0bd411b412c62c1f22f33170
장유진 <danya6@naver.com>
2024-08-01 17:49:11 +0900

    feat: 헤더 레이아웃 컴포넌트 생성

---
+++ src/layout/header/HeaderStyle.js
@@ -0,0 +1,50 @@
+import styled from '@emotion/styled';
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  height: 3.125rem;
+  position: fixed;
+  top: 0;
+  display: flex;
+  align-items: center;
+  background: #FF6969;
+
+  .header-content {
+    width: 95%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .Button {
+    width: 2.1875rem;
+    height: 2.1875rem;
+    background: none;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .IconContainer {
+    display: flex;
+  }
+
+  .Title {
+    color: #FFF;
+    text-align: center;
+    font-family: "KoddiUD OnGothic";
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+
+  .icon {
+    width: 25px;
+    height: 25px;
+    vertical-align: middle;
+  }
+`;
+
+export default HeaderContainer;
