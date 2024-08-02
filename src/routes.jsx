da0a5cf13ada5fd70b39d8b8d4a75d5c30ee387b
백하람 <developer.venice@gmail.com>
2024-08-02 13:12:07 +0900

    feat: 모든 페이지 router 정의 및 페이지 만들기
    애플리케이션의 모든 페이지의 router를 정의하고,
    각 라우터 당 매핑되는 페이지를 생성하였습니다.
    페이지 레이아웃에 걸맞는 UI를 가질 수 있도록 index.css를 수정하였습니다.
    
    14

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,34 @@
 import { Routes, Route } from "react-router-dom";
-import PageLayout from "./layout/PageLayout";
-import LandingPage from "./pages/LandingPage";
+import PageLayout from "./layout/pageLayout/PageLayout";
+import LandingPage from "./pages/landing/LandingPage";
+import LandingPageLayout from "./layout/landingPageLayout/LandingPageLayout";
+import SignInPage from "./pages/signin/SignInPage";
+import SignUpPage from "./pages/signup/SignUpPage";
+import ClosetsPage from "./pages/closets/ClosetsPage";
+import ClothesListPage from "./pages/clothes/list/ClothesListPage";
+import ClothesDetailPage from "./pages/clothes/detail/ClothesDetailPage";
+import SearchPage from "./pages/search/SearchPage";
+import BookMarkListPage from "./pages/bookmarks/list/BookMarkListPage";
+import BookMarkDetailPage from "./pages/bookmarks/detail/BookMarkDetailPage";
+import UserPage from "./pages/user/view/UserPage";
+import PasswordPage from "./pages/user/password/PasswordPage";
+import NicknamePage from "./pages/user/nickname/NicknamePage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<PageLayout page={<LandingPage />} />} />
+      <Route path="/" element={<LandingPageLayout page={<LandingPage />} />} />
+      <Route path="/signin" element={<LandingPageLayout page={<SignInPage />} />} />
+      <Route path="/signup" element={<LandingPageLayout page={<SignUpPage />} />} />
+      <Route path="/closets" element={<PageLayout page={<ClosetsPage />} />} />
+      <Route path="/clothes" element={<PageLayout page={<ClothesListPage />} />} />
+      <Route path="/clothes/:id" element={<PageLayout page={<ClothesDetailPage />} />} />
+      <Route path="/search" element={<PageLayout page={<SearchPage />} />} />
+      <Route path="/bookmarks" element={<PageLayout page={<BookMarkListPage />} />} />
+      <Route path="/bookmarks/:id" element={<PageLayout page={<BookMarkDetailPage />} />} />
+      <Route path="/user" element={<PageLayout page={<UserPage />} />} />
+      <Route path="/nickname" element={<PageLayout page={<NicknamePage />} />} />
+      <Route path="/password" element={<PageLayout page={<PasswordPage />} />} />
     </Routes>
   );
 };
