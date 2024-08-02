1c5653cf4d1500916d52ecb6c9d2704eead9ccf6
michael <oilpott12@naver.com>
2024-08-02 17:03:59 +0900

    refactor: onclick -> onTouchStart로 변경

--- src/components/socialbutton/SocialButtonStyle.js
+++ src/components/socialbutton/SocialButtonStyle.js
@@ -11,6 +11,8 @@ const SocialButtonStyle = styled.div`
 
     cursor: pointer;
 
+    -webkit-tap-highlight-color: transparent;
+
     &:focus {
         box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.4) inset;
         
