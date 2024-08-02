a0ed35636374938b7097cc3241729238e98ddef0
michael <oilpott12@naver.com>
2024-08-02 22:43:54 +0900

    feat: 검색 카드 컴포넌트 생성
    prop
    searchCardName
    searchCardLocation
    ouTouchStart

---
+++ frontend/CloseToYou/src/components/searchCard/SearchCard.jsx
@@ -0,0 +1,19 @@
+import SearchCardStyle from './SearchCardStyle';
+
+const SearchCard = ({ searchCardName, searchCardLocation, onTouchStart }) => {
+    return(
+        <SearchCardStyle onTouchStart={ onTouchStart } tabIndex={0}>
+            <div className="search-card-left-box">
+                <div className="search-card-icon-box">
+                    <div className="search-card-icon"></div>
+                </div>
+            </div>
+            <div className="search-card-right-box">
+                <div className="search-card-name">{ searchCardName }</div>
+                <div className="search-card-location">{ searchCardLocation }</div>
+            </div>
+        </SearchCardStyle>
+    );
+}
+
+export default SearchCard;
