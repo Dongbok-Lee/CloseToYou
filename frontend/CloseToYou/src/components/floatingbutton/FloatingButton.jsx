ce8ffec019cc41b713036091f024cad7de8c2e7b
장유진 <danya6@naver.com>
2024-08-02 17:09:29 +0900

    refactor: 플로팅버튼 컴포넌트 onClick이벤트 수정

--- frontend/CloseToYou/src/components/floatingbutton/FloatingButton.jsx
+++ frontend/CloseToYou/src/components/floatingbutton/FloatingButton.jsx
@@ -1,7 +1,7 @@
-import { ButtonContainer } from "././FloatingButtonStyle";
-import AddIcon from "../../assets/icons/etc/plus-small.svg";
-import DeleteIcon from "../../assets/icons/etc/delete.svg";
-import EditIcon from "../../assets/icons/etc/edit.svg";
+import { ButtonContainer } from '././FloatingButtonStyle';
+import AddIcon from '../../assets/icons/etc/plus-small.svg';
+import DeleteIcon from '../../assets/icons/etc/delete.svg';
+import EditIcon from '../../assets/icons/etc/edit.svg'
 
 const icons = {
   add: AddIcon,
@@ -11,8 +11,8 @@ const icons = {
 
 const FloatingButton = ({ type, handleClick }) => {
   return (
-    <ButtonContainer onTouchStart={() => handleClick()} isAddType={type === "add"} tabIndex={0}>
-      <img src={icons[type]} alt={`${type} icon`} style={{ width: "2rem", height: "2rem" }} />
+    <ButtonContainer onTouchStart={() => handleClick()} isAddType={type === 'add'} tabIndex={0}>
+      <img src={icons[type]} alt={`${type} icon`} style={{ width: '2rem', height: '2rem' }} />
     </ButtonContainer>
   );
 };
