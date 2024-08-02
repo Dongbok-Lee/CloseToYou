8b7f2cb69c4ff40fe3d485c8298e5338222a0ab2
장유진 <danya6@naver.com>
2024-08-02 09:31:03 +0900

    refactor: 헤더 레이아웃 컴포넌트 함수이름 수정

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -7,21 +7,21 @@ const icons = {
   search: SearchIcon,
 };
 
-const Header = ({ title, onBack, onSearch }) => {
-  const handleIconClick = (type) => {
-    if (type === 'back') onBack();
-    else if (type === 'search') onSearch();
+const Header = ({ title, onClickBack, onClickSearch }) => {
+  const handleClickIcon = (type) => {
+    if (type === 'back') onClickBack();
+    else if (type === 'search') onClickSearch();
   };
 
   return (
     <HeaderContainer>
       <div style={{ width: "95%", margin: "auto", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <button className="Button" onClick={() => handleIconClick('back')} aria-label="뒤로가기">
+        <button className="Button" onClick={() => handleClickIcon('back')} aria-label="뒤로가기">
           <img src={icons.back} alt="뒤로가기" style={{ width: '25px', height: '25px', verticalAlign: 'middle' }} />
         </button>
         <span className="Title">{title}</span>
         <div className="IconContainer">
-          <button className="Button" onClick={() => handleIconClick('search')} aria-label="검색">
+          <button className="Button" onClick={() => handleClickIcon('search')} aria-label="검색">
             <img src={icons.search} alt="검색" style={{ width: '25px', height: '25px', verticalAlign: 'middle' }} />
           </button>
         </div>
