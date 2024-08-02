d468eea33af88a91e52536a4938a91763ae66367
백하람 <developer.venice@gmail.com>
2024-08-02 13:12:07 +0900

    feat: 모든 페이지 router 정의 및 페이지 만들기
    애플리케이션의 모든 페이지의 router를 정의하고,
    각 라우터 당 매핑되는 페이지를 생성하였습니다.
    페이지 레이아웃에 걸맞는 UI를 가질 수 있도록 index.css를 수정하였습니다.
    
    14

---
+++ src/pages/user/nickname/NicknamePage.jsx
@@ -0,0 +1,7 @@
+import { NicknamePageContainer } from "./NicknamePageStyle";
+
+const NicknamePage = () => {
+  return <NicknamePageContainer className="page">NicknamePage</NicknamePageContainer>;
+};
+
+export default NicknamePage;
