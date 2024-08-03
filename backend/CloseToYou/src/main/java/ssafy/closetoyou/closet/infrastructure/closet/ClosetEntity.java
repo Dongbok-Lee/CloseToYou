c7bb7fffc00c0a032336244c8dd9608e78c0b5a1
이수민 <gogotnals@naver.com>
2024-08-03 21:35:41 +0900

    feat: Closet 레포지토리 구현

---
+++ backend/CloseToYou/src/main/java/ssafy/closetoyou/closet/infrastructure/closet/ClosetEntity.java
@@ -0,0 +1,65 @@
+package ssafy.closetoyou.closet.infrastructure.closet;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import ssafy.closetoyou.closet.domain.Closet;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "closets")
+@Getter @Setter @ToString
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+public class ClosetEntity {
+
+    @Id
+    @GeneratedValue
+    private Long closetId;
+
+    private Long userId;
+    private String closetCode;
+    private String nickname;
+    private Boolean isDeleted = false;
+
+    @CreatedDate
+    LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    LocalDateTime updatedDateTime;
+
+    @Builder
+    public ClosetEntity(Long closetId, Long userId, String closetCode, String nickname, Boolean isDeleted, LocalDateTime createdDateTime, LocalDateTime updatedDateTime) {
+        this.closetId = closetId;
+        this.userId = userId;
+        this.closetCode = closetCode;
+        this.nickname = nickname;
+        this.isDeleted = isDeleted;
+        this.createdDateTime = createdDateTime;
+        this.updatedDateTime = updatedDateTime;
+    }
+
+    public static ClosetEntity fromModel(Closet closet) {
+        return ClosetEntity.builder()
+                .closetId(closet.getClosetId())
+                .userId(closet.getUserId())
+                .closetCode(closet.getClosetCode())
+                .nickname(closet.getNickname())
+                .isDeleted(false)
+                .build();
+    }
+
+    public Closet toModel() {
+        return Closet.builder()
+                .closetId(closetId)
+                .userId(userId)
+                .closetCode(closetCode)
+                .nickname(nickname)
+                .isDeleted(isDeleted)
+                .createdDateTime(createdDateTime)
+                .updateDateTime(updatedDateTime)
+                .build();
+    }
+}
