7bb1663d1dca4d25569870226df0354c92330626
백하람 <developer.venice@gmail.com>
2024-08-04 12:24:09 +0900

    rename: 폴더 구조 병합
    - root/frontend/CloseToYou/프로젝트
    root/src => 이런 식으로 튀어나와 있던 폴더 프로젝트 내부로 이동
    - .gitignore 파일 프로젝트 root 경로로 이동

--- frontend/CloseToYou/src/pages/bookmarks/detail/BookMarkDetailPage.jsx
+++ frontend/CloseToYou/src/pages/bookmarks/detail/BookMarkDetailPage.jsx
@@ -1,7 +1,9 @@
 import { BookMarkDetailPageContainer } from "./BookMarkDetailPageStyle";
 
 const BookMarkDetailPage = () => {
-  return <BookMarkDetailPageContainer className="page">BookMarkDetailPage</BookMarkDetailPageContainer>;
+  return (
+    <BookMarkDetailPageContainer className="page">BookMarkDetailPage</BookMarkDetailPageContainer>
+  );
 };
 
 export default BookMarkDetailPage;
