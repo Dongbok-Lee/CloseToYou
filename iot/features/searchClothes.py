2603340056f21c3230006e50a8a6ac1d1f404534
chpark6851 <chpark6851@koreatech.ac.kr>
2024-08-04 17:01:48 +0900

    feat : 옷 등록 모듈 작성 및 센서 수정
    - 마이크로 말한 문장을 띄어쓰기마다 문자열을 잘라서 문자열 배열로
      반환하도록 수정
    - nfc 리더기에 nfc 센서가 5초 간 태깅되지 않으면 예외처리

--- iot/features/searchClothes.py
+++ iot/features/searchClothes.py
@@ -6,16 +6,18 @@ from speaker import text_to_speech
 
 def searchClothes():
     while True:
-        text = recognize_speech_from_mic()
-        if(text is None):
+        words = recognize_speech_from_mic()
+        if(words is None):
             voice="띠링"
             text_to_speech(voice)
             return
 
+        text = wordsToText(words)
         if(text=="옷 찾아 줘"):
             voice="어떤 옷을 찾아 드릴까요?"
             text_to_speech(voice)
-            text = recognize_speech_from_mic()
+            words = recognize_speech_from_mic()
+            text=wordsToText(words)
             voice=text+"는 A-3 에 위치해 있습니다."
             text_to_speech(voice)
 
@@ -24,6 +26,9 @@ def searchClothes():
             voice="다시 말씀해 주세요"
             text_to_speech(voice)
 
+
+def wordsToText(words):
+    return ' '.join(words)
 
 if __name__ == "__main__":
     searchClothes()
