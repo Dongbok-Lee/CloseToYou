7aace38b624084f4f9a87f39849442e51df63738
chpark6851 <chpark6851@koreatech.ac.kr>
2024-08-04 20:45:21 +0900

    feat : 옷 등록 NFC 연결 및 DB 데이터 입력

--- iot/features/registClothes.py
+++ iot/features/registClothes.py
@@ -8,6 +8,8 @@ from nfc import read_nfc
 from adafruit_pn532.i2c import PN532_I2C
 import busio
 import board
+sys.path.append('/home/orin/S11P12B201/iot/database/')
+from mySqlConnection import read_db_config, MySQLDatabase
 
 def registClothes(pn532):
     voice="촬영용 거치대에 옷을 걸어주세요."
@@ -29,17 +31,28 @@ def registClothes(pn532):
         return
 
     print(uid)
+
+    insert_query = "INSERT INTO clothes (nickname, color, location) VALUES (%s, %s, %s)"
+    db.execute_query(insert_query, ("파란 무지 반팦티", "파란색","B-1"))
+
+    voice = "옷이 정상적으로 등록되었습니다. 옷장에 걸어주세요."
+    text_to_speech(voice)
+
+
+
     # else:
 
 
 if __name__ == "__main__":
-    print("########################################1")
     i2c = busio.I2C(board.SCL, board.SDA)
-    print("#######################################2")
     pn532 = PN532_I2C(i2c, debug=False)
-    print("#######################################3")
     ic, ver, rev, support = pn532.firmware_version
     pn532.SAM_configuration()
-    print("#######################################4")
-    registClothes(pn532)
 
+    # DB 연결
+    config = read_db_config()
+    db = MySQLDatabase(config)
+    db.connect()
+
+    registClothes(pn532)
+    db.disconnect()
