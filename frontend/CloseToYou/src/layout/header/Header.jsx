67fde88ee4f875d05e00804befc1924ae312e16c
백하람 <developer.venice@gmail.com>
2024-08-04 12:24:09 +0900

    rename: 폴더 구조 병합
    - root/frontend/CloseToYou/프로젝트
    root/src => 이런 식으로 튀어나와 있던 폴더 프로젝트 내부로 이동
    - .gitignore 파일 프로젝트 root 경로로 이동

---
+++ frontend/CloseToYou/src/layout/header/Header.jsx
@@ -0,0 +1,49 @@
+import HeaderContainer from "./HeaderStyle";
+import BackIcon from "../../assets/icons/etc/back.svg";
+import SearchIcon from "../../assets/icons/etc/search.svg";
+
+const icons = {
+  back: BackIcon,
+  search: SearchIcon,
+};
+
+const Header = ({ title, onClickBack, onClickSearch }) => {
+  const handleClickIcon = type => {
+    if (type === "back") onClickBack();
+    else if (type === "search") onClickSearch();
+  };
+
+  return (
+    <HeaderContainer>
+      <div
+        style={{
+          width: "95%",
+          margin: "auto",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <button className="Button" onClick={() => handleClickIcon("back")} aria-label="뒤로가기">
+          <img
+            src={icons.back}
+            alt="뒤로가기"
+            style={{ width: "25px", height: "25px", verticalAlign: "middle" }}
+          />
+        </button>
+        <span className="Title">{title}</span>
+        <div className="IconContainer">
+          <button className="Button" onClick={() => handleClickIcon("search")} aria-label="검색">
+            <img
+              src={icons.search}
+              alt="검색"
+              style={{ width: "25px", height: "25px", verticalAlign: "middle" }}
+            />
+          </button>
+        </div>
+      </div>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
