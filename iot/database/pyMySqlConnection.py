709837e8199f5667eac574961eb10ca6f16c65a3
이동복 <typoon0820@gmail.com>
2024-08-04 21:04:58 +0900

    feat: ai연동 검색기능 추가

---
+++ iot/database/pyMySqlConnection.py
@@ -0,0 +1,28 @@
+import pymysql.cursors
+
+# 데이터베이스 연결 정보 설정
+
+
+def execute_query(query):
+    connection = pymysql.connect(
+        host='127.0.0.1',       # 데이터베이스 호스트 이름 또는 IP 주소
+        user='root',   # 데이터베이스 사용자 이름
+        password='typoon0820', # 데이터베이스 사용자 비밀번호
+        database='ctytest', # 연결할 데이터베이스 이름
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    result = ""
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            connection.commit()
+
+            result = cursor.fetchall()
+            print(result)
+    finally:
+        # 연결 종료
+        connection.close()
+    return result
+
+print(execute_query("insert into clothes(nickname, type, pattern, color, location) values ('하양 티셔츠', '티셔츠', '없음', '하양', 'a9')"))
+print(execute_query("select * from clothes"))
