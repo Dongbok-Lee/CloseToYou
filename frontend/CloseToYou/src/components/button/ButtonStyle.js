ef19b5cdbfa59b9a4eb233862c955a12856f12ba
백하람 <developer.venice@gmail.com>
2024-08-05 17:28:02 +0900

    fix: conflict 해결

--- frontend/CloseToYou/src/components/button/ButtonStyle.js
+++ frontend/CloseToYou/src/components/button/ButtonStyle.js
@@ -1,12 +1,11 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
 
 const ButtonStyle = styled.button`
-
   box-sizing: border-box;
 
   border-radius: 0.625rem;
   border-color: rgba(255, 255, 255, 0);
-  
+
   width: 18.75rem;
   min-height: 3.75rem;
 
@@ -24,15 +23,19 @@ const ButtonStyle = styled.button`
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.4) inset;
     outline: none;
   }
-  
-  ${({ btnSize }) => btnSize === 'small' && `
+
+  ${({ btnSize }) =>
+    btnSize === "small" &&
+    `
     width: 12.5rem;
     height: 3.125rem;
 
     font-size: 1.75rem;
   `}
 
-  ${({ btnColor }) => btnColor === 'white' && `
+  ${({ btnColor }) =>
+    btnColor === "white" &&
+    `
     border: 2px solid rgba(255, 105, 105, 1);
 
     background-color: rgba(255, 255, 255, 1);
@@ -40,7 +43,9 @@ const ButtonStyle = styled.button`
     color: rgba(255, 105, 105, 1);
   `}
 
-  ${({ btnColor }) => btnColor === 'gray' && `
+  ${({ btnColor }) =>
+    btnColor === "gray" &&
+    `
     border: 2px solid rgba(0, 0, 0, 1);
     
     background-color: rgba(255, 255, 255, 1);
--- frontend/CloseToYou/src/components/searchbox/SearchBox.jsx
+++ frontend/CloseToYou/src/components/searchbox/SearchBox.jsx
@@ -1,13 +1,13 @@
 /** @jsxImportSource @emotion/react */
-import { useState } from 'react';
-import { SearchBoxContainer } from './SearchBoxStyle';
-import SearchBoxIcon from '../../assets/icons/etc/searchbox.svg';
+import { useState } from "react";
+import { SearchBoxContainer } from "./SearchBoxStyle";
+import SearchBoxIcon from "../../assets/icons/etc/searchbox.svg";
 
-const SearchBox = ({ value = '', onSearch }) => {
+const SearchBox = ({ value = "", onSearch }) => {
   const [inputValue, setInputValue] = useState(value);
   const [isFocused, setIsFocused] = useState(false);
 
-  const handleChangeInput = (e) => {
+  const handleChangeInput = e => {
     setInputValue(e.target.value);
   };
 
@@ -15,8 +15,8 @@ const SearchBox = ({ value = '', onSearch }) => {
     onSearch(inputValue);
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
+  const handleKeyDown = e => {
+    if (e.key === "Enter") {
       handleClickSearch();
     }
   };
@@ -40,7 +40,7 @@ const SearchBox = ({ value = '', onSearch }) => {
         onFocus={handleFocus}
         onBlur={handleBlur}
       />
-      <div className="IconContainer" onClick={handleClickSearch}>
+      <div className="IconContainer" onTouchStart={handleClickSearch}>
         <img src={SearchBoxIcon} alt="검색 아이콘" className="icon" />
       </div>
     </SearchBoxContainer>
