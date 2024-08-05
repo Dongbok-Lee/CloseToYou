ffa657f5a5008e410f1798419e3200756f358760
백하람 <developer.venice@gmail.com>
2024-08-05 16:37:26 +0900

    feat: 마이페이지 UI 개발

--- frontend/CloseToYou/src/pages/user/view/UserPage.jsx
+++ frontend/CloseToYou/src/pages/user/view/UserPage.jsx
@@ -1,7 +1,73 @@
-import { UserPageContainer } from "./UserPageStyle";
+import {
+  ButtonText,
+  ChangeBox,
+  HighContrastContainer,
+  UserInfoContainer,
+  UserPageContainer,
+  WelcomeContainer,
+} from "./UserPageStyle";
+import { ToggleContainer } from "../../../components/togglebutton/ToggleButtonStyle.js";
+import { useEffect, useState } from "react";
+import ToggleButton from "../../../components/togglebutton/ToggleButton.jsx";
+import { useNavigate } from "react-router-dom";
+
+// TODO: GET, /api/users
+const user = {
+  email: "chano@gmail.com",
+  nickname: "기가차노",
+  registTime: "",
+  updateTime: "",
+  isHighContrast: false,
+};
 
 const UserPage = () => {
-  return <UserPageContainer className="page">UserPage</UserPageContainer>;
+  const [isToggle, setIsToggle] = useState(false);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    setIsToggle(user.isHighContrast);
+  }, []);
+
+  const handleTouchChangeNickname = e => {
+    navigate("/nickname");
+  };
+  const handleTouchChangePassword = () => {
+    navigate("/password");
+  };
+
+  const handleTouchLogout = e => {
+    e.target.focus();
+    window.confirm("정말 로그아웃 하세요?🥺");
+  };
+
+  return (
+    <UserPageContainer className="page">
+      <HighContrastContainer>
+        <span>고대비 모드</span>
+        <ToggleButton isOn={isToggle} setIsOn={setIsToggle} />
+      </HighContrastContainer>
+      <WelcomeContainer>
+        <span className="title-text">{user.nickname}</span>
+        <span className="sub-text">님, 안녕하세요!</span>
+      </WelcomeContainer>
+      <UserInfoContainer>
+        <p className="title-text">이메일</p>
+        <p className="sub-text">{user.email}</p>
+      </UserInfoContainer>
+      <UserInfoContainer>
+        <p className="title-text">닉네임</p>
+        <ChangeBox onTouchStart={handleTouchChangeNickname}>
+          <p className="sub-text">{user.nickname}</p>
+          <ButtonText>변경</ButtonText>
+        </ChangeBox>
+      </UserInfoContainer>
+      <ChangeBox>
+        <p className="sub-text">비밀 번호</p>
+        <ButtonText onTouchStart={handleTouchChangePassword}>변경</ButtonText>
+      </ChangeBox>
+      <ButtonText onTouchStart={handleTouchLogout}>로그아웃</ButtonText>
+    </UserPageContainer>
+  );
 };
 
 export default UserPage;
--- frontend/CloseToYou/src/pages/user/view/UserPageStyle.js
+++ frontend/CloseToYou/src/pages/user/view/UserPageStyle.js
@@ -1,3 +1,64 @@
 import styled from "@emotion/styled";
 
-export const UserPageContainer = styled.div``;
+export const UserPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 75vh;
+  gap: 3rem;
+
+  p {
+    margin: 0;
+  }
+
+  .title-text {
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
+
+  .sub-text {
+    font-weight: 400;
+    font-size: 1.25rem;
+  }
+`;
+
+export const HighContrastContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 1rem;
+
+  span {
+    font-size: 1.5rem;
+  }
+`;
+
+export const WelcomeContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+export const UserInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const ChangeBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ButtonText = styled.p`
+  font-weight: 700;
+  font-size: 1.5rem;
+  color: #ff6969;
+  text-align: center;
+  cursor: pointer;
+
+  &:focus {
+    text-decoration-line: underline;
+  }
+`;
