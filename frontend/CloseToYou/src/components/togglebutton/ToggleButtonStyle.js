3f676e34a959aa8e6c93a236b707dd588c2f61bc
michael <oilpott12@naver.com>
2024-08-05 10:51:57 +0900

    style: 코드 포맷팅

--- frontend/CloseToYou/src/components/togglebutton/ToggleButtonStyle.js
+++ frontend/CloseToYou/src/components/togglebutton/ToggleButtonStyle.js
@@ -1,5 +1,5 @@
-import styled from '@emotion/styled';
-import { keyframes } from '@emotion/react';
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
 
 const toggleOn = keyframes`
   0% { transform: translateX(0.125rem); }
@@ -14,8 +14,8 @@ const toggleOff = keyframes`
 export const ToggleContainer = styled.div`
   width: 3.75rem;
   height: 1.875rem;
-  background: ${(props) => (props.isOn ? '#FF6969' : '#FFF')};
-  border: 2px solid ${(props) => (props.isOn ? '#FFF' : '#FF6969')};
+  background: ${props => (props.isOn ? "#FF6969" : "#FFF")};
+  border: 2px solid ${props => (props.isOn ? "#FFF" : "#FF6969")};
   border-radius: 1.875rem;
   display: flex;
   align-items: center;
@@ -26,22 +26,22 @@ export const ToggleContainer = styled.div`
   .toggle-circle {
     width: 1.375rem;
     height: 1.375rem;
-    background: #FFF;
-    border: 2px solid #FF6969;
+    background: #fff;
+    border: 2px solid #ff6969;
     border-radius: 50%;
     margin-left: 0.125rem;
     transform: translateX(0.125rem);
-    animation: ${(props) => (props.isOn ? toggleOn : toggleOff)} 0.3s forwards;
+    animation: ${props => (props.isOn ? toggleOn : toggleOff)} 0.3s forwards;
   }
 `;
 
 export const ToggleCircle = styled.div`
   width: 1.375rem;
   height: 1.375rem;
-  background: #FFF;
-  border: 2px solid #FF6969;
+  background: #fff;
+  border: 2px solid #ff6969;
   border-radius: 50%;
   margin-left: 0.125rem;
   transform: translateX(0.125rem);
-  animation: ${(props) => (props.isOn ? toggleOn : toggleOff)} 0.3s forwards;
+  animation: ${props => (props.isOn ? toggleOn : toggleOff)} 0.3s forwards;
 `;
