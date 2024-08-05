ea76f5d6a156db2d1baa1dda008235903c39e00c
michael <oilpott12@naver.com>
2024-08-05 10:54:26 +0900

    feat: 모달 터치 핸들러 추가

--- frontend/CloseToYou/src/components/modal/Modal.jsx
+++ frontend/CloseToYou/src/components/modal/Modal.jsx
@@ -2,26 +2,44 @@ import ModalStyle from './ModalStyle';
 import Button from '../button/Button';
 import Input from '../input/Input';
 
-const Modal = ({ modalType, modalSize, onTouchStart, children }) => {
+const Modal = ({ modalType, modalSize, modalPlaceholderFirst, modalPlaceholderSecond, setIsOpenModal, children }) => {
 
     const deleteText = "정말 삭제 하시겠어요?";
+
+    const handleTouchCloseIcon = (e) => {
+        e.target.focus();
+
+        setTimeout(() => {
+            e.target.blur();
+            setIsOpenModal(false);
+        },100)
+    }
+
+    const handleTouchButton = (e) => {
+        e.target.focus();
+
+        setTimeout(() => {
+            e.target.blur();
+            setIsOpenModal(false);
+        },100)
+    }
 
     return(
             <ModalStyle modalType={ modalType } modalSize={ modalSize }>
                 <div className="modal-container">
                     <div className="modal-up-box">                
                         <div className="close-icon-box">
-                            <div className="close-icon" tabIndex={0} onTouchStart={ onTouchStart }></div>
+                            <div className="close-icon" onTouchStart={ handleTouchCloseIcon } tabIndex={0} ></div>
                         </div>
                     </div>
                     <div className="modal-down-box">
                         <div className="modal-down-input">
                             { modalType === 'delete' && <div className="modal-delete-text">{ deleteText }</div>}
-                            { modalSize === 'large' && modalType !== 'delete' && <Input inputSize="medium"></Input>}
-                            { modalType !== 'delete' && <Input inputSize="medium"></Input>}
+                            { modalSize === 'large' && modalType !== 'delete' && <Input inputSize="medium" inputPlaceholder={ modalPlaceholderSecond }></Input>}
+                            { modalType !== 'delete' && <Input inputSize="medium" inputPlaceholder={ modalPlaceholderFirst }></Input>}
                         </div>
                         <div className="modal-down-button">
-                            <Button btnSize='small' onTouchStart={ onTouchStart }>{ children }</Button> 
+                            <Button btnSize='small' onTouchStart={ handleTouchButton }>{ children }</Button> 
                         </div>
                     </div>
                 </div>
