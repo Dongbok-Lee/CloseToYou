897efbe7aaed597963f3e6f27eda788f9ca9cd47
michael <oilpott12@naver.com>
2024-08-05 11:10:03 +0900

    feat: select 컴포넌트 생성
    미완성으로 추후 변경 예정

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react'
+
 import SelectStyle from './SelectStyle';
 
-const Select = ({ children }) => {
+const Select = ({ initItem }) => {
 
     const items = ["빨간색", "노란색", "주황색", "초록색", "파란색", "보라색", "갈색"];
 
+    const [ isTouched, setIsTouched ] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(initItem);
+    const [ isFocused, setIsFocused ] = useState(false);
+
+    const handleTouchSelectBox = () => {
+        setIsTouched(true);
+
+        setTimeout(() => {
+            setIsFocused(true);
+        }, 50)
+    }
+
+    const handleTouchOption = (item) => {
+        setSelectedItem(item);
+
+        setIsTouched(false);
+
+        setTimeout(() => {
+            setIsFocused(false);
+        }, 50)
+    }
+
     return(
-        <SelectStyle>
-            <div className="select-up-box" tabIndex={0}>
-                <div className="select-up-title">{ children }</div>
+        <SelectStyle isFocused={ isFocused }>
+            <div className="select-up-box" onTouchStart={ handleTouchSelectBox } tabIndex={0}>
+                <div className="select-up-title">{ selectedItem }</div>
                 <div className="select-up-icon-box">
                     <div className="select-up-icon"></div>
                 </div>
@@ -15,7 +39,7 @@ const Select = ({ children }) => {
             <div className="select-down-box">
                 <ul className="select-list">
                     {items.map((item, index) => (
-                        <li className="select-list-item" key={ index } tabIndex={0}>{ item }</li>
+                        <li className="select-list-item" key={ index } onTouchStart={() => handleTouchOption(item) } tabIndex={0}>{ item }</li>
                     ))}
                 </ul>
             </div>
