873ea1a2f422b9f5997f508e434ba089a446948c
장유진 <danya6@naver.com>
2024-08-08 14:10:38 +0900

    fix: routes 주소 추가 및 header 수정

--- frontend/CloseToYou/src/routes.jsx
+++ frontend/CloseToYou/src/routes.jsx
@@ -7,7 +7,8 @@ import SignUpPage from "./pages/signup/SignUpPage";
 import ClosetsPage from "./pages/closets/ClosetsPage";
 import ClothesListPage from "./pages/clothes/list/ClothesListPage";
 import ClothesDetailPage from "./pages/clothes/detail/ClothesDetailPage";
-import ClothesEditPage from "./pages/clothes/detail/ClothesEditPage";
+import ClothesNfcPage from "./pages/clothes/nfc/ClothesNfcPage";
+import ClothesEditPage from "./pages/clothes/edit/ClothesEditPage";
 import SearchPage from "./pages/search/SearchPage";
 import BookMarkListPage from "./pages/bookmarks/list/BookMarkListPage";
 import BookMarkDetailPage from "./pages/bookmarks/detail/BookMarkDetailPage";
@@ -24,7 +25,8 @@ const AppRoutes = () => {
       <Route path="/closets" element={<PageLayout page={<ClosetsPage />} />} />
       <Route path="/clothes" element={<PageLayout page={<ClothesListPage />} />} />
       <Route path="/clothes/:id" element={<PageLayout page={<ClothesDetailPage />} />} />
-      <Route path="/clothes/edit/0" element={<PageLayout page={<ClothesEditPage />} />} />
+      <Route path="/clothes/nfc/:id" element={<PageLayout page={<ClothesNfcPage />} />} />
+      <Route path="/clothes/edit/:id" element={<PageLayout page={<ClothesEditPage />} />} />
       <Route path="/search" element={<PageLayout page={<SearchPage />} />} />
       <Route path="/bookmarks" element={<PageLayout page={<BookMarkListPage />} />} />
       <Route path="/bookmarks/:id" element={<PageLayout page={<BookMarkDetailPage />} />} />
