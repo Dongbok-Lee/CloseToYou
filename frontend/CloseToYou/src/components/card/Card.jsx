828f7b628ee9998584414cc2de8696c9d85e7ae3
백하람 <developer.venice@gmail.com>
2024-08-09 15:55:29 +0900

    design: 카드 컴포넌트 디자인 수정

--- frontend/CloseToYou/src/components/card/Card.jsx
+++ frontend/CloseToYou/src/components/card/Card.jsx
@@ -1,13 +1,8 @@
 import { CardContainer } from "./CardStyle";
 
-const Card = ({ handleTouch, handleDoubleClick, Icon, FocusIcon, isFocused }) => {
+const Card = ({ handleTouch, Icon, FocusIcon, isFocused }) => {
   return (
-    <CardContainer
-      onTouchStart={handleTouch}
-      onDoubleClick={handleDoubleClick}
-      tabIndex={0}
-      isFocused={isFocused}
-    >
+    <CardContainer onTouchStart={handleTouch} tabIndex={0} isFocused={isFocused}>
       {isFocused ? <FocusIcon /> : <Icon />}
     </CardContainer>
   );
