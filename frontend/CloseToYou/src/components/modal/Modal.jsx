df1494351a522b7418cbb4cdeaab186631707138
michael <oilpott12@naver.com>
2024-08-10 23:13:12 +0900

    fix: 모달 컴포넌트 닫기 버튼 핸들 함수에 파라미터 e 추가
    모달 닫기 버튼 핸들 함수에 파라미터가 없어서 작동 않아서 수정

--- frontend/CloseToYou/src/components/modal/Modal.jsx
+++ frontend/CloseToYou/src/components/modal/Modal.jsx
@@ -1,70 +1,88 @@
-import {CloseImg, CloseImgBox, ContentWrapper, DeleteText, TextInputWrapper, ModalContainer, ModalWrapper, TextBox} from "./ModalStyle"
+import {
+  CloseImg,
+  CloseImgBox,
+  ContentWrapper,
+  DeleteText,
+  TextInputWrapper,
+  ModalContainer,
+  ModalWrapper,
+  TextBox,
+} from "./ModalStyle";
 import TextInput from "../textinput/TextInput";
 import Button from "../button/Button";
 
 const Modal = ({
-    modalType,
-    modalSize,
-    firstPlaceholder,
-    secondPlaceholder,
-    setIsOpenModal,
-    setFirstValue,
-    setSecondValue,
-    children
+  modalType,
+  modalSize,
+  firstPlaceholder,
+  secondPlaceholder,
+  setIsOpenModal,
+  setFirstValue,
+  setSecondValue,
+  children,
 }) => {
+  const deleteMessage = "정말 삭제하시겠습니까?";
 
-    const deleteMessage = "정말 삭제하시겠습니까?";
+  const handleChangeFirstValue = e => {
+    setFirstValue(e.target.value);
+  };
 
-    const handleChangeFirstValue = (e) => {
-      setFirstValue(e.target.value);
-    }
+  const handleChangeSecondValue = e => {
+    setSecondValue(e.target.value);
+  };
 
-    const handleChangeSecondValue = (e) => {
-      setSecondValue(e.target.value)
-    }
+  const handleTouchClose = e => {
+    e.target.focus();
 
-    const handleTouchClose = () => {
-      e.target.focus();
+    setTimeout(() => {
+      setIsOpenModal(false);
+    }, 100);
+  };
 
-      setTimeout(() => {
-        setIsOpenModal(false);
-      }, 100)
-    }
+  const handleTouchButton = e => {
+    e.target.focus();
 
-    const handleTouchButton = (e) => {
-      e.target.focus();
+    setTimeout(() => {
+      setIsOpenModal(false);
+    }, 100);
+  };
 
-      setTimeout(() => {
-        setIsOpenModal(false);
-      }, 100)
-    }
-
-    return(
-            <ModalContainer>
-                <ModalWrapper size={modalSize}>
-                    <CloseImgBox>
-                        <CloseImg src="src/assets/icons/etc/close-button.svg" onTouchStart={handleTouchClose} tabIndex={0}></CloseImg>
-                    </CloseImgBox>
-                    <ContentWrapper>
-                        {modalType === "delete" ?
-                        <TextBox>
-                            <DeleteText>{deleteMessage}</DeleteText>
-                        </TextBox>
-                        :
-                        <TextInputWrapper>
-                            {modalSize !== "" &&
-                                <TextInput textInputPlaceholder={firstPlaceholder} handleChangeTextInput={handleChangeFirstValue}></TextInput>
-                            }
-                            {modalSize === "large" &&
-                                <TextInput textInputPlaceholder={secondPlaceholder} handleChangeTextInput={handleChangeSecondValue}></TextInput>
-                            }
-                        </TextInputWrapper>
-                        }
-                        <Button handleTouchButton={handleTouchButton}>{children}</Button>
-                    </ContentWrapper>
-                </ModalWrapper>
-            </ModalContainer>       
-    );
-}
+  return (
+    <ModalContainer>
+      <ModalWrapper size={modalSize}>
+        <CloseImgBox>
+          <CloseImg
+            src="src/assets/icons/etc/close-button.svg"
+            onTouchStart={handleTouchClose}
+            tabIndex={0}
+          ></CloseImg>
+        </CloseImgBox>
+        <ContentWrapper>
+          {modalType === "delete" ? (
+            <TextBox>
+              <DeleteText>{deleteMessage}</DeleteText>
+            </TextBox>
+          ) : (
+            <TextInputWrapper>
+              {modalSize !== "" && (
+                <TextInput
+                  textInputPlaceholder={firstPlaceholder}
+                  handleChangeTextInput={handleChangeFirstValue}
+                ></TextInput>
+              )}
+              {modalSize === "large" && (
+                <TextInput
+                  textInputPlaceholder={secondPlaceholder}
+                  handleChangeTextInput={handleChangeSecondValue}
+                ></TextInput>
+              )}
+            </TextInputWrapper>
+          )}
+          <Button handleTouchButton={handleTouchButton}>{children}</Button>
+        </ContentWrapper>
+      </ModalWrapper>
+    </ModalContainer>
+  );
+};
 
 export default Modal;
