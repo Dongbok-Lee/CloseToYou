540d0ad92b2ea59ef59d5d33a673b34972949c21
michael <oilpott12@naver.com>
2024-08-12 03:32:10 +0900

    feat: 회원가입 api 구현

--- frontend/CloseToYou/src/stores/user.jsx
+++ frontend/CloseToYou/src/stores/user.jsx
@@ -1,6 +1,38 @@
 import { create } from "zustand";
+import { createUser } from "../api/user";
 
-const useStore = create(set => ({
-  user: { email: "", nickname: "" },
-  isHighContrast: false,
+export const useUserStore = create(set => ({
+  nickname: null,
+  email: null,
+  password: null,
+  nicknameResponse: null,
+  passwordResponse: null,
+  signUpResponse: null,
+
+  addUser: async (nickname, email, password) => {
+    set({ nickname: nickname, email: email, password: password });
+
+    await createUser(nickname, email, password)
+      .then(response => {
+        if (response.status === 200) {
+          set({ signUpResponse: response.data.message });
+        }
+      })
+      .catch(error => {
+        if (error.response.status === 400) {
+          const errors = error.response.data.errors;
+          errors.forEach(item => {
+            if (item.field === "nickname") {
+              set({ nicknameResponse: item.message });
+            }
+
+            if (item.field === "password") {
+              set({ passwordResponse: item.message });
+            }
+          });
+        }
+      });
+  },
+  setNicknameResponse: newNicknameResponse => set({ nicknameResponse: newNicknameResponse }),
+  setPasswordResponse: newPasswordResponse => set({ passwordResponse: newPasswordResponse }),
 }));
