e2223373d86ab6b2972f0069cb98aa012f8b45c7
이수민 <gogotnals@naver.com>
2024-08-11 18:01:38 +0900

    fix: Swagger https 도메인 허용

--- backend/CloseToYou/src/main/java/ssafy/closetoyou/ClosetoyouApplication.java
+++ backend/CloseToYou/src/main/java/ssafy/closetoyou/ClosetoyouApplication.java
@@ -1,11 +1,14 @@
 package ssafy.closetoyou;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {@Server(url = "https://i11b201.p.ssafy.io", description = "Default Server URL")})
 public class ClosetoyouApplication {
 
 	public static void main(String[] args) {
