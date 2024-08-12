10137e9739fb5ae28bde7893d950b6649829e1ca
michael <oilpott12@naver.com>
2024-08-12 15:05:16 +0900

    feat: 로그인 api 구현

--- frontend/CloseToYou/src/api/user.js
+++ frontend/CloseToYou/src/api/user.js
@@ -9,3 +9,12 @@ export const createUser = async (nickname, email, password) => {
 
   return response;
 };
+
+export const createSignIn = async (email, password) => {
+  const response = await clientInstance.post("api/users/login", {
+    email: email,
+    password: password,
+  });
+
+  return response;
+};
