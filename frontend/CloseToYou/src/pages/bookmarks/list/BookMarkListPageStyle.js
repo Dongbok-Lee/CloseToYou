693928b24c1d585e5c778bc77179796ad1a27651
백하람 <developer.venice@gmail.com>
2024-08-12 23:17:06 +0900

    feat: bookmark delete, add api 연동 및 기능 구현

--- frontend/CloseToYou/src/pages/bookmarks/list/BookMarkListPageStyle.js
+++ frontend/CloseToYou/src/pages/bookmarks/list/BookMarkListPageStyle.js
@@ -13,11 +13,11 @@ export const BookMarkListPageContainer = styled.div`
 `;
 
 export const TextWrapper = styled.div`
-  margin: 2.5rem  0 1.5rem 0;
+  margin: 2.5rem 0 1.5rem 0;
   height: 3.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export const TitleText = styled.p`
--- frontend/CloseToYou/src/stores/bookmark.jsx
+++ frontend/CloseToYou/src/stores/bookmark.jsx
@@ -1,25 +1,44 @@
-import { create } from "zustand";
-import { getBookmarkList } from "../api/bookmark.js";
+import {create} from "zustand";
+import {createBookmark, deleteBookmark, getBookmarkList} from "../api/bookmark.js";
 
 const useBookmarkStore = create(set => ({
-  bookmarkList: [],
-  bookmark: {
-    bookmarkId: 0,
-    nickname: "",
-    userId: 0,
-  },
-  error: null,
-  loading: false,
+    bookmarkList: [],
+    bookmark: {
+        bookmarkId: 0,
+        nickname: "",
+        userId: 0,
+    },
+    error: null,
+    loading: true,
 
-  loadBookmarkList: async () => {
-    const { data, status } = await getBookmarkList();
-    if (status === 200) {
-      set({ bookmarkList: data.data });
-      set({ loading: true });
-    } else {
-      set({ error: data });
+    loadBookmarkList: async () => {
+        const {data, status} = await getBookmarkList();
+        if (status === 200) {
+            set({bookmarkList: data.data});
+            set({loading: false});
+        } else {
+            set({error: data});
+        }
+    },
+
+    removeBookmark: async (bookmarkId) => {
+        const {data, status} = await deleteBookmark(bookmarkId);
+        if (status === 200) {
+            set({loading: false})
+        } else {
+            set({error: data});
+        }
+    },
+
+    addBookmark: async (nickname) => {
+        const {data, status} = await createBookmark(nickname);
+        if (status === 200) {
+            set({loading: false});
+        } else {
+
+            set({error: data});
+        }
     }
-  },
 }));
 
 export default useBookmarkStore;
