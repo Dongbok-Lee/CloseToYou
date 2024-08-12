d7ae3963bfac51e0a624f067c72b8cb004205edc
백하람 <developer.venice@gmail.com>
2024-08-13 05:23:13 +0900

    feat: 사용자 정보 조회 및 고대비 모드 기능 구현 중

--- frontend/CloseToYou/src/pages/user/view/UserViewPage.jsx
+++ frontend/CloseToYou/src/pages/user/view/UserViewPage.jsx
@@ -8,27 +8,25 @@ import {
   UserPageContainer,
   WelcomeWrapper,
 } from "./UserViewPageStyle.js";
-import { ToggleContainer } from "../../../components/togglebutton/ToggleButtonStyle.js";
 import { useEffect, useState } from "react";
 import ToggleButton from "../../../components/togglebutton/ToggleButton.jsx";
 import { useNavigate } from "react-router-dom";
-
-// TODO: GET, /api/users
-const user = {
-  email: "chano@gmail.com",
-  nickname: "기가차노",
-  registTime: "",
-  updateTime: "",
-  isHighContrast: false,
-};
+import { useUserStore } from "../../../stores/user.jsx";
 
 const UserViewPage = () => {
   const [isToggle, setIsToggle] = useState(false);
 
+  const { nickname, email, isHighContrast, loadUserInfo, editHighContrast } = useUserStore();
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    setIsToggle(user.isHighContrast);
+    // Cookies.set(
+    //   "accessToken",
+    //   "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcyMzQ5NTU0OSwiZW1haWwiOiJocmIuaGFycGVyQGdtYWlsLmNvbSIsImlkIjoxM30.Q4xqqDSdYwnhwgC2cF6VheRFR_7k5CJtb-vbbkRs6_i3VeTsn0J1M0HYOvw8vL8fC7Nao8xVXVVjBbgXxIQBgQ",
+    // );
+    (!nickname || !email) && loadUserInfo();
+    setIsToggle(isHighContrast);
   }, []);
 
   const handleTouchChangeNickname = e => {
@@ -49,18 +47,18 @@ const UserViewPage = () => {
         <span>고대비 모드</span>
         <ToggleButton isOn={isToggle} setIsOn={setIsToggle} />
       </HighContrastWrapper>
-      <WelcomeWrapper tabIndex={0} title={`${user.nickname}님, 안녕하세요.`}>
-        <TitleText>{user.nickname}</TitleText>
+      <WelcomeWrapper tabIndex={0} title={`${nickname}님, 안녕하세요.`}>
+        <TitleText>{nickname}</TitleText>
         <SubText>님, 안녕하세요!</SubText>
       </WelcomeWrapper>
       <UserInfoWrapper>
         <TitleText tabIndex={0}>이메일</TitleText>
-        <SubText tabIndex={0}>{user.email}</SubText>
+        <SubText tabIndex={0}>{email}</SubText>
       </UserInfoWrapper>
       <UserInfoWrapper>
         <TitleText tabIndex={0}>닉네임</TitleText>
         <ChangeWrapper>
-          <SubText tabIndex={0}>{user.nickname}</SubText>
+          <SubText tabIndex={0}>{nickname}</SubText>
           <ButtonText onTouchStart={handleTouchChangeNickname} tabIndex={0}>
             변경
           </ButtonText>
