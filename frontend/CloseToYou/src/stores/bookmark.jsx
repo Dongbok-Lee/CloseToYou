4a6c5282bcda0cad5aa509790045549c9b19265e
백하람 <developer.venice@gmail.com>
2024-08-13 13:31:36 +0900

    fix: bookmark list 페이지 탭 키 눌렀을 때 발생하는 오류 해결

--- frontend/CloseToYou/src/stores/bookmark.jsx
+++ frontend/CloseToYou/src/stores/bookmark.jsx
@@ -1,4 +1,4 @@
-import { create } from "zustand";
+import {create} from "zustand";
 import {
   createBookmark,
   deleteBookmark,
@@ -9,77 +9,77 @@ import {
 } from "../api/bookmark.js";
 
 const useBookmarkStore = create(set => ({
-  bookmarkList: [],
-  bookmark: {
-    bookmarkId: 0,
-    nickname: "",
-    clothes: [
-      {
-        clothesId: 0,
+    bookmarkList: [],
+    bookmark: {
+        bookmarkId: 0,
         nickname: "",
-        color: "",
-        type: "",
-      },
-    ],
-  },
-  error: null,
-  loading: true,
+        clothes: [
+            {
+                clothesId: 0,
+                nickname: "",
+                color: "",
+                type: "",
+            },
+        ],
+    },
+    error: null,
+    loading: true,
 
-  loadBookmarkList: async () => {
-    const { data, status } = await getBookmarkList();
-    if (status === 200) {
-      set({ bookmarkList: data.data });
-      set({ loading: false });
-    } else {
-      set({ error: data });
-    }
-  },
+    loadBookmarkList: async () => {
+        const {data, status} = await getBookmarkList();
+        if (status === 200) {
+            set({bookmarkList: data.data});
+            set({loading: false});
+        } else {
+            set({error: data});
+        }
+    },
 
-  removeBookmark: async bookmarkId => {
-    const { data, status } = await deleteBookmark(bookmarkId);
-    if (status === 200) {
-      set({ loading: false });
-    } else {
-      set({ error: data });
-    }
-  },
+    removeBookmark: async bookmarkId => {
+        const {data, status} = await deleteBookmark(bookmarkId);
+        if (status === 200) {
+            set({loading: false});
+        } else {
+            set({error: data});
+        }
+    },
 
-  addBookmark: async newNickname => {
-    const { data, status } = await createBookmark(newNickname);
-    if (status === 200) {
-      set({ loading: false });
-    } else {
-      set({ error: data });
-    }
-  },
+    addBookmark: async newNickname => {
+        const {data, status} = await createBookmark(newNickname);
+        if (status === 200) {
+            set({loading: false});
+        } else {
+            set({error: data});
+        }
+    },
 
-  editBookmark: async (bookmarkId, modifiedNickname) => {
-    const { data, status } = await patchBookmark(bookmarkId, modifiedNickname);
-    if (status === 200) {
-      set({ loading: false });
-    } else {
-      set({ error: data });
-    }
-  },
+    editBookmark: async (bookmarkId, modifiedNickname) => {
+        const {data, status} = await patchBookmark(bookmarkId, modifiedNickname);
+        if (status === 200) {
+            set({loading: false});
+        } else {
+            set({error: data});
+        }
+    },
 
-  loadBookmarkDetail: async bookmarkId => {
-    const { data, status } = await getBookmarkDetail(bookmarkId);
-    if (status === 200) {
-      set({ loading: false });
-      set({ bookmark: data.data });
-    } else {
-      set({ error: data });
-    }
-  },
+    loadBookmarkDetail: async bookmarkId => {
+        const {data, status} = await getBookmarkDetail(bookmarkId);
+        if (status === 200) {
+            set({loading: false});
+            set({bookmark: data.data});
+        } else {
+            set({error: data});
+        }
+    },
 
-  removeClothesInBookmark: async (bookmarkId, clothesId) => {
-    const { data, status } = await deleteClothesInBookmark(bookmarkId, clothesId);
-    if (status === 200) {
-      set({ loading: false });
-    } else {
-      set({ error: data });
-    }
-  },
+    removeClothesInBookmark: async (bookmarkId, clothesId) => {
+        const {data, status} = await deleteClothesInBookmark(bookmarkId, clothesId);
+        if (status === 200) {
+            set({loading: false});
+        } else {
+            set({error: data});
+        }
+    },
 }));
 
 export default useBookmarkStore;
--- frontend/CloseToYou/src/utils/http-client.js
+++ frontend/CloseToYou/src/utils/http-client.js
@@ -2,56 +2,56 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const createClientInstance = () => {
-  const instance = axios.create({
-    baseURL: import.meta.env.VITE_REST_API_URL,
-    timeout: 5000,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": import.meta.env.VITE_CLIENT_URL,
-      "Access-Control-Allow-Credentials": "true",
-    },
-    withCredentials: true,
-  });
+    const instance = axios.create({
+        baseURL: import.meta.env.VITE_REST_API_URL,
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": import.meta.env.VITE_CLIENT_URL,
+            "Access-Control-Allow-Credentials": "true",
+        },
+        withCredentials: true,
+    });
 
-  return instance;
+    return instance;
 };
 
 const createAuthClientInstance = () => {
-  const accessToken = Cookies.get("access_token");
+    const accessToken = Cookies.get("access_token");
 
-  const instance = axios.create({
-    baseURL: import.meta.env.VITE_REST_API_URL,
-    timeout: 5000,
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`,
-      "Access-Control-Allow-Origin": import.meta.env.VITE_CLIENT_URL,
-      "Access-Control-Allow-Credentials": "true",
-    },
-    withCredentials: true,
-  });
+    const instance = axios.create({
+        baseURL: import.meta.env.VITE_REST_API_URL,
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcyMzUyNjA0NSwiZW1haWwiOiJocmIuaGFycGVyQGdtYWlsLmNvbSIsImlkIjoxM30.XlVNOFvmn0gXVcaJ7cj8zOB9iHsUZ9y2K3gb1lgiQHZnPMeSsLiZiGuZC1yCCyD6nmHAvHe5EAkxOtXeRQW2OQ`,
+            "Access-Control-Allow-Origin": import.meta.env.VITE_CLIENT_URL,
+            "Access-Control-Allow-Credentials": "true",
+        },
+        withCredentials: true,
+    });
 
-  return instance;
+    return instance;
 };
 
 const createAuthWithRefreshClientInstance = () => {
-  const accessToken = Cookies.get("access_token");
-  const refreshToken = Cookies.get("refresh_token");
+    const accessToken = Cookies.get("access_token");
+    const refreshToken = Cookies.get("refresh_token");
 
-  const instance = axios.create({
-    baseURL: import.meta.env.VITE_REST_API_URL,
-    timeout: 5000,
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`,
-      "Authorization-refresh": `Bearer ${refreshToken}`,
-      "Access-Control-Allow-Origin": import.meta.env.VITE_CLIENT_URL,
-      "Access-Control-Allow-Credentials": "true",
-    },
-    withCredentials: true,
-  });
+    const instance = axios.create({
+        baseURL: import.meta.env.VITE_REST_API_URL,
+        timeout: 5000,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${accessToken}`,
+            "Authorization-refresh": `Bearer ${refreshToken}`,
+            "Access-Control-Allow-Origin": import.meta.env.VITE_CLIENT_URL,
+            "Access-Control-Allow-Credentials": "true",
+        },
+        withCredentials: true,
+    });
 
-  return instance;
+    return instance;
 };
 
 export const clientInstance = createClientInstance();
