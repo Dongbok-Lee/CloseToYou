38252f6413e25dfc3496cb7a9f03b0d18d35f995
michael <oilpott12@naver.com>
2024-08-13 16:40:11 +0900

    feat: 옷장페이지 api 구현

--- frontend/CloseToYou/src/stores/closet.jsx
+++ frontend/CloseToYou/src/stores/closet.jsx
@@ -1,14 +1,60 @@
 import { create } from "zustand";
+import { getClosets, createClosets, deleteClosets, patchClosets } from "../api/closet";
 
-const useStore = create(set => ({
-  closetList: [
-    {
-      closetId: 0,
-      nickname: "",
-      userId: 0,
-      closetCode: "",
-      clothesCount: 0,
-    },
-  ],
-  getClosetList: state => state.closetList,
+export const useClosetsStore = create((set, get) => ({
+  closetId: "",
+  nickname: "",
+  closetCode: "",
+  clothesCount: 0,
+  closets: [],
+
+  loadClosets: async () => {
+    await getClosets()
+      .then(response => {
+        set({ closets: response.data.data });
+        console.log(response.data.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+
+  addClosets: async (nickname, closetCode) => {
+    set({ nickname: nickname, closetCode: closetCode });
+
+    await createClosets(nickname, closetCode)
+      .then(async response => {
+        await get().loadClosets();
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+
+  removeClosets: async closetId => {
+    set({ closetId: closetId });
+
+    await deleteClosets(closetId)
+      .then(async response => {
+        await get().loadClosets();
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+
+  editClosets: async (closetId, nickname) => {
+    set({ closetId: closetId, nickname: nickname });
+
+    await patchClosets(closetId, nickname)
+      .then(async response => {
+        await get().loadClosets();
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
 }));
