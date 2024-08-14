4dcfde63fe5e807926c3220b229a5148fc4bf4f7
백하람 <developer.venice@gmail.com>
2024-08-14 10:56:20 +0900

    fix: bookmark detail 페이지 API 연동

--- frontend/CloseToYou/src/stores/bookmark.jsx
+++ frontend/CloseToYou/src/stores/bookmark.jsx
@@ -1,6 +1,7 @@
-import {create} from "zustand";
+import { create } from "zustand";
 import {
   createBookmark,
+  createclothesInBookmark,
   deleteBookmark,
   deleteClothesInBookmark,
   getBookmarkDetail,
@@ -9,77 +10,86 @@ import {
 } from "../api/bookmark.js";
 
 const useBookmarkStore = create(set => ({
-    bookmarkList: [],
-    bookmark: {
-        bookmarkId: 0,
+  bookmarkList: [],
+  bookmark: {
+    bookmarkId: 0,
+    nickname: "",
+    clothes: [
+      {
+        clothesId: 0,
         nickname: "",
-        clothes: [
-            {
-                clothesId: 0,
-                nickname: "",
-                color: "",
-                type: "",
-            },
-        ],
-    },
-    error: null,
-    loading: true,
+        color: "",
+        type: "",
+      },
+    ],
+  },
+  error: null,
+  loading: true,
 
-    loadBookmarkList: async () => {
-        const {data, status} = await getBookmarkList();
-        if (status === 200) {
-            set({bookmarkList: data.data});
-            set({loading: false});
-        } else {
-            set({error: data});
-        }
-    },
+  loadBookmarkList: async () => {
+    const { data, status } = await getBookmarkList();
+    if (status === 200) {
+      set({ bookmarkList: data.data });
+      set({ loading: false });
+    } else {
+      set({ error: data });
+    }
+  },
 
-    removeBookmark: async bookmarkId => {
-        const {data, status} = await deleteBookmark(bookmarkId);
-        if (status === 200) {
-            set({loading: false});
-        } else {
-            set({error: data});
-        }
-    },
+  removeBookmark: async bookmarkId => {
+    const { data, status } = await deleteBookmark(bookmarkId);
+    if (status === 200) {
+      set({ loading: false });
+    } else {
+      set({ error: data });
+    }
+  },
 
-    addBookmark: async newNickname => {
-        const {data, status} = await createBookmark(newNickname);
-        if (status === 200) {
-            set({loading: false});
-        } else {
-            set({error: data});
-        }
-    },
+  addBookmark: async newNickname => {
+    const { data, status } = await createBookmark(newNickname);
+    if (status === 200) {
+      set({ loading: false });
+    } else {
+      set({ error: data });
+    }
+  },
 
-    editBookmark: async (bookmarkId, modifiedNickname) => {
-        const {data, status} = await patchBookmark(bookmarkId, modifiedNickname);
-        if (status === 200) {
-            set({loading: false});
-        } else {
-            set({error: data});
-        }
-    },
+  editBookmark: async (bookmarkId, modifiedNickname) => {
+    const { data, status } = await patchBookmark(bookmarkId, modifiedNickname);
+    if (status === 200) {
+      set({ loading: false });
+    } else {
+      set({ error: data });
+    }
+  },
 
-    loadBookmarkDetail: async bookmarkId => {
-        const {data, status} = await getBookmarkDetail(bookmarkId);
-        if (status === 200) {
-            set({loading: false});
-            set({bookmark: data.data});
-        } else {
-            set({error: data});
-        }
-    },
+  loadBookmarkDetail: async bookmarkId => {
+    const { data, status } = await getBookmarkDetail(bookmarkId);
+    if (status === 200) {
+      set({ loading: false });
+      set({ bookmark: data.data });
+    } else {
+      set({ error: data });
+    }
+  },
 
-    removeClothesInBookmark: async (bookmarkId, clothesId) => {
-        const {data, status} = await deleteClothesInBookmark(bookmarkId, clothesId);
-        if (status === 200) {
-            set({loading: false});
-        } else {
-            set({error: data});
-        }
-    },
+  addClothesInBookmark: async (bookmarkId, clothesId) => {
+    const { data, status } = await createclothesInBookmark(bookmarkId, clothesId);
+    if (status === 200) {
+      set({ loading: false });
+    } else {
+      set({ error: data });
+    }
+  },
+
+  removeClothesInBookmark: async (bookmarkId, clothesId) => {
+    const { data, status } = await deleteClothesInBookmark(bookmarkId, clothesId);
+    if (status === 200) {
+      set({ loading: false });
+    } else {
+      set({ error: data });
+    }
+  },
 }));
 
 export default useBookmarkStore;
