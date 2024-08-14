addcae7a8b0d0c96dd35bcb54d3a07cb4a0b9cc1
michael <oilpott12@naver.com>
2024-08-14 22:58:40 +0900

    style: 북마크 상세 스타일 변경

--- frontend/CloseToYou/src/pages/bookmarks/detail/BookMarkDetailPageStyle.js
+++ frontend/CloseToYou/src/pages/bookmarks/detail/BookMarkDetailPageStyle.js
@@ -1,6 +1,12 @@
 import styled from "@emotion/styled";
 
-export const BookMarkDetailPageContainer = styled.div``;
+export const BookMarkDetailPageContainer = styled.div`
+  display: flex;
+
+  flex-direction: column;
+
+  min-heigth: 75vh;
+`;
 
 export const BookmarkNickname = styled.p`
   text-align: center;
@@ -14,4 +20,8 @@ export const CardNickname = styled.p`
   text-align: center;
   font-size: 1.75rem;
   font-weight: 700;
+`;
+
+export const FloatingButtonWraper = styled`
+  margin: 4rem 0 1.25rem 0;
 `;
