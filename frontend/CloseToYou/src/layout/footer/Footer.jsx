cf7eba4951f9a9c72e00f621b94230daeaa0ce2e
장유진 <danya6@naver.com>
2024-08-15 18:38:08 +0900

    fix: footer 원상복구

--- frontend/CloseToYou/src/layout/footer/Footer.jsx
+++ frontend/CloseToYou/src/layout/footer/Footer.jsx
@@ -2,25 +2,32 @@ import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import {
-  BookmarkImg,
-  BookmarkText,
-  BookmarkWrapper,
+  FooterContainer,
   ClosetImg,
-  ClosetText,
   ClosetWrapper,
+  ClosetText,
+  ClothesWrapper,
   ClothesImg,
   ClothesText,
-  ClothesWrapper,
-  FooterContainer,
+  BookmarkWrapper,
+  BookmarkImg,
+  BookmarkText,
+  ProfileWrapper,
   ProfileImg,
   ProfileText,
-  ProfileWrapper,
 } from "./FooterStyle";
+
+import ClosetBaiscImg from "../../assets/icons/etc/closet-basic.svg";
+import ClosetFocusImg from "../../assets/icons/etc/closet-focus.svg";
+import ClothesBasicImg from "../../assets/icons/etc/hanger-basic.svg";
+import ClothesFocusImg from "../../assets/icons/etc/hanger-focus.svg";
+import BookmarkBasicImg from "../../assets/icons/etc/bookmark-basic.svg";
+import BookmarkFocusImg from "../../assets/icons/etc/bookmark-focus.svg";
+import ProfileBasicImg from "../../assets/icons/etc/profile-basic.svg";
+import ProfileFocusImg from "../../assets/icons/etc/profile-focus.svg";
 
 const Footer = () => {
   const nav = useNavigate();
-  const theme = useTheme();
-  const { mode } = theme;
 
   let location = useLocation();
 
@@ -28,36 +35,34 @@ const Footer = () => {
 
   useEffect(() => {
     setUrl(location.pathname);
+    console.log(location.pathname);
   }, [location.pathname]);
 
-
-  const handleTouchCloset = (e) => {
+  const handleTouchCloset = e => {
     setTimeout(() => {
       document.activeElement.blur();
       nav("/closets");
     }, 150);
   };
 
-  const handleTouchClothes = (e) => {
+  const handleTouchClothes = e => {
     setTimeout(() => {
       document.activeElement.blur();
       nav("/clothes");
     }, 150);
   };
 
-  const handleTouchBookmark = (e) => {
+  const handleTouchBookmark = e => {
     setTimeout(() => {
       document.activeElement.blur();
       nav("/bookmarks");
-
     }, 150);
   };
 
-  const handleTouchProfile = (e) => {
+  const handleTouchProfile = e => {
     setTimeout(() => {
       document.activeElement.blur();
       nav("/user");
-
     }, 150);
   };
 
@@ -65,57 +70,33 @@ const Footer = () => {
     <FooterContainer>
       <ClosetWrapper tabIndex={0} onTouchStart={handleTouchCloset}>
         {url.includes("/closets") ? (
-          <ClosetImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/closet-focus.svg"
-            alt="옷장으로 바로가기"
-          ></ClosetImg>
+          <ClosetImg src={ClosetFocusImg} alt="옷장으로 바로가기"></ClosetImg>
         ) : (
-          <ClosetImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/closet-basic.svg"
-            alt="옷장으로 바로가기"
-          ></ClosetImg>
+          <ClosetImg src={ClosetBaiscImg} alt="옷장으로 바로가기"></ClosetImg>
         )}
         <ClosetText url={url}>옷장</ClosetText>
       </ClosetWrapper>
       <ClothesWrapper tabIndex={0} onTouchStart={handleTouchClothes}>
         {url.includes("/clothes") ? (
-          <ClothesImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/hanger-focus.svg"
-            alt="옷으로 바로가기"
-          ></ClothesImg>
+          <ClothesImg src={ClothesFocusImg} alt="옷으로 바로가기"></ClothesImg>
         ) : (
-          <ClothesImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/hanger-basic.svg"
-            alt="옷으로 바로가기"
-          ></ClothesImg>
+          <ClothesImg src={ClothesBasicImg} alt="옷으로 바로가기"></ClothesImg>
         )}
         <ClothesText url={url}>옷</ClothesText>
       </ClothesWrapper>
       <BookmarkWrapper tabIndex={0} onTouchStart={handleTouchBookmark}>
         {url.includes("/bookmarks") ? (
-          <BookmarkImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/bookmark-focus.svg"
-            alt="북마크로 바로가기"
-          ></BookmarkImg>
+          <BookmarkImg src={BookmarkFocusImg} alt="북마크로 바로가기"></BookmarkImg>
         ) : (
-          <BookmarkImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/bookmark-basic.svg"
-            alt="북마크로 바로가기"
-          ></BookmarkImg>
+          <BookmarkImg src={BookmarkBasicImg} alt="북마크로 바로가기"></BookmarkImg>
         )}
         <BookmarkText url={url}>북마크</BookmarkText>
       </BookmarkWrapper>
       <ProfileWrapper tabIndex={0} onTouchStart={handleTouchProfile}>
         {url.includes("/user") || url.includes("/nickname") || url.includes("/password") ? (
-          <ProfileImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/profile-focus.svg"
-            alt="프로필로 바로가기"
-          ></ProfileImg>
+          <ProfileImg src={ProfileFocusImg} alt="프로필로 바로가기"></ProfileImg>
         ) : (
-          <ProfileImg
-            src="https://i11b201.p.ssafy.io/assets/icons/etc/profile-basic.svg"
-            alt="프로필로 바로가기"
-          ></ProfileImg>
+          <ProfileImg src={ProfileBasicImg} alt="프로필로 바로가기"></ProfileImg>
         )}
         <ProfileText url={url}>프로필</ProfileText>
       </ProfileWrapper>
