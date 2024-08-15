50be427bb6a057237f0a6075d97af1bb244e788c
michael <oilpott12@naver.com>
2024-08-15 21:02:38 +0900

    feat: 소셜 로그인 처리

--- frontend/CloseToYou/src/pages/landing/LandingPage.jsx
+++ frontend/CloseToYou/src/pages/landing/LandingPage.jsx
@@ -1,14 +1,25 @@
 import { AppTitleText, LandigPageContainer } from "./LandingPageStyle";
 import Logo from "../../assets/logo.svg?react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const LandingPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+
+  const accessToken = query.get("accessToken");
+
+  if (accessToken) {
+    sessionStorage.setItem("accessToken", accessToken);
+
+    navigate("/closets", { replace: true });
+  } else {
     setTimeout(() => {
-        // TODO: 로그인 여부 확인해서 로그인 화면 또는 옷장 고르는 화면으로 이동
-        navigate('/signin');
-    }, 3000)
+      navigate("/signin");
+    }, 3000);
+  }
+
   return (
     <LandigPageContainer title="Close To You 애플리케이션의 랜딩 화면 입니다.">
       <Logo />
